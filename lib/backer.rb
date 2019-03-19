dcf232d4afd5ea510949db712026501e74a29f75
Donna Amos <donnacamos.developer@gmail.com>
2019-03-19 21:15:01 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,14 +1,13 @@
-class Backer 
-  
-  attr_accessor :name, :backed_projects  
-  
+class Backer
+  attr_accessor :name, :backed_projects
+
   def initialize(name)
-    @name = name 
+    @name = name
     @backed_projects = []
-  end 
-  
-  def backed_project(project) 
-    @backed_projects << projects 
-    project.add_backer(self) unless project.backer.include?(self) 
-  end 
-end 
+  end
+
+  def back_project(project)
+    @backed_projects << project
+    project.add_backer(self) unless project.backers.include?(self)
+  end
+end
